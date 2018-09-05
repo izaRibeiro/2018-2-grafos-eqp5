3e7e30dc0f12d9a61a01a5abc8997385e3fc9baf
viictorw <viictor.wagner@gmail.com>
2018-09-05 12:16:28 -0300

    Começo

---
+++ TAF1-Grafos/src/GrafoMatrizIncidencia.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Scanner;
+
+public class GrafoMatrizIncidencia {
+    Scanner scanner = new Scanner(System.in);
+
+    private int qtdeVertice;
+    private int qtdeArestas;
+    private int[][] matrizIncidencia;
+
+    public void matrizNaoOrientada() {
+        pegarInformacao();
+        matrizIncidencia = new int[qtdeVertice][qtdeArestas];// vertice | aresta
+        for (int i = 0; i < qtdeVertice; i++) {
+            for (int j = 0; j < qtdeArestas; j++) {
+                System.out.printf("Vertice %d se liga com a aresta %d\n", i + 1, j + 1);
+                System.out.println("s - sim | n - não");
+                char op = scanner.next().charAt(0);
+                switch (op) {
+                    case 's' :
+                        matrizIncidencia[i][j] = 1;
+                        break;
+                    case 'n':
+                        matrizIncidencia[i][j] = 0;
+                        break;
+                            }
+
+            }
+        }
+    }

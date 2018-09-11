bc05cb99815ddcb6146507400ddaa5622afb69b0
Izabella Ribeiro <bellarpjf@gmail.com>
2018-09-10 23:05:20 -0300

    Up

---
+++ GerenciamentoDeEspaços/src/model/Usuario.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private Long id;
+    private String dataNascimento;
+    private Double altura;
+    private Double peso;
+
+
+}

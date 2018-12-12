571737d33310935e3b5fd990477652d0c402dc61
luis <luisguilhermeguimaraes@hotmail.com>
2018-12-12 02:22:00 -0200

    Update GrafoToDot.java

--- Grafos/src/model/GrafoToDot.java
+++ Grafos/src/model/GrafoToDot.java
@@ -47,4 +47,57 @@ public class GrafoToDot {
           return graphDot;
     } 
   
+    public static Grafo importaGrafoDot(String graphDot){
+        Grafo grafo = new Grafo();
+        Aresta aresta;
+     if(grafo.iseOrientado()){
+           graphDot = "Digraph ";
+           
+       }else{
+           graphDot = "Graph ";
+       }
+      int i;
+        String aux, aux2;
+        String[] arrayAux;
+        String[] arrayGrafo = graphDot.split("\\{\n");
+
+        //pegando o nome do grafo pelo espaço entre o tipo do grafo e o nome
+        grafo.setId(arrayGrafo[0].split(" ")[1]);
+
+        //filtrando os vértices
+        arrayAux = arrayGrafo[1].split(";\n ")[0].split(";\n");
+
+        for (i = 0; i < arrayAux.length; i++) {
+            if (arrayAux[i].contains("@@")) {
+                arrayAux[i].replace(" ", "@@");
+            }
+            grafo.adicionaVertice(arrayAux[i]);
+        }
+
+        arrayGrafo = arrayGrafo[1].split(";\n ");
+
+        arrayAux = arrayGrafo[1].split("];\n\n ");
+
+        if (grafo.iseOrientado()) {
+            aux = "->";
+        } else {
+            aux = "--";
+        }
+
+        for (i = 0; i < arrayAux.length - 1; i++) {
+            Vertice origem = new Vertice(arrayAux[i].split(aux)[0].trim());
+            Vertice destino = new Vertice(arrayAux[i].split(aux)[1].split("\\[")[0].trim());
+
+            aresta = new Aresta(origem, destino);
+            aresta.setId(arrayAux[i].split("name = \"")[1].split("\"")[0]);
+            if (arrayAux[i].contains("label")) {
+                aresta.setPeso(Double.parseDouble(arrayAux[i].split("label = \"")[1].split("\"")[0]));
+            }
+            aresta.setDirecionado(grafo.iseOrientado());
+            grafo.adicionaAresta(aresta);
+        }
+        grafo.verificaTudo();
+        return grafo;
+    }
+    
 }
